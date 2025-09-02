bb75a413def70a653be3ebb01ee0fd7b91284ad2
Diogo Porto <diogo.g.porto@gmail.com>
2025-09-02 19:31:10 +0000

    first commit

--- RPS.py
+++ RPS.py
@@ -3,16 +3,23 @@ import RPS_game
 
 def player(prev_play, opponent_history=[]):
     counter = {'R': 'P', 'P': 'S', 'S': 'R'}
-    guess = "R"
+    choice = ["R", "P", "S"]
+    guess = choice[len(opponent_history) % 2]
     #quincy
     if opponent_history[:5] == ["", "R", "P", "P", "S"]:
         guess = counter[RPS_game.quincy(prev_play)]
         return guess
     #mrugesh
-    elif opponent_history[:2] == ["S", "R"]:
+    elif opponent_history[:2] == ["", "P", "P", "P"]:
         guess = counter[RPS_game.mrugesh(prev_play)]
         return guess
-
+    #kris
+    elif opponent_history[:5] == ["", "P", "S", "R", "P", "S"]:
+        guess = counter[RPS_game.kris(prev_play)]
+        return guess
+    #abbey
+    else:
+        guess = counter[RPS_game.abbey(prev_play)]
     opponent_history.append(prev_play)
     return guess
 
